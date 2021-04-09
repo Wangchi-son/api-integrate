51d7dc8f95b63c2b624b71cb7f2877b91f499c8a
unknown <sonwj0915@naver.com>
2021-04-09 20:48:21 +0900

    id 값을 받아와 따로 표시하게 하기

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import useAsync from "./useAsync";
+
+import User from "./User";
 
 async function getUsers() {
   const response = await axios.get("http://jsonplaceholder.typicode.com/users");
@@ -9,6 +11,7 @@ async function getUsers() {
 
 function Users() {
   const [state, refetch] = useAsync(getUsers, [], true);
+  const [userId, setUserId] = useState(null);
 
   const { loading, data: users, error } = state;
   if (loading) return <div>로딩중...</div>;
@@ -19,12 +22,13 @@ function Users() {
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li key={user.id} onClick={() => setUserId(user.id)}>
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
       <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
